cfbb25e0d96d3e4445f4a8284fc8546f1759a636
Matas Jovaiša <jovaisamatas@gmail.com>
2021-12-29 23:41:07 +0200

    Initial setup.

---
+++ register/Controllers/EntriesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using register.Models;
+
+namespace register.Controllers
+{
+    public class EntriesController : Controller
+    {
+        private registerContext db = new registerContext();
+
+        // GET: Entries
+        public ActionResult Index()
+        {
+            return View(db.Entries.ToList());
+        }
+
+        // GET: Entries/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Entry entry = db.Entries.Find(id);
+            if (entry == null)
+            {
+                return HttpNotFound();
+            }
+            return View(entry);
+        }
+
+        // GET: Entries/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Entries/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Attribute,Value")] Entry entry)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entries.Add(entry);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(entry);
+        }
+
+        // GET: Entries/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Entry entry = db.Entries.Find(id);
+            if (entry == null)
+            {
+                return HttpNotFound();
+            }
+            return View(entry);
+        }
+
+        // POST: Entries/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Attribute,Value")] Entry entry)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(entry).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(entry);
+        }
+
+        // GET: Entries/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Entry entry = db.Entries.Find(id);
+            if (entry == null)
+            {
+                return HttpNotFound();
+            }
+            return View(entry);
+        }
+
+        // POST: Entries/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Entry entry = db.Entries.Find(id);
+            db.Entries.Remove(entry);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ register/Models/Entry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace register.Models
+{
+    public class Entry
+    {
+        public int ID { get; set; }
+        public string Attribute { get; set; }
+        public string Value { get; set; }
+
+        public Entry()
+        {
+
+        }
+    }
+}
